2f7975ed750556f9a9633717b9f967183bd37fa8
hahahafeifeifei <569631224@qq.com>
2025-12-01 01:10:52 +0800

    update build script

--- build.sh
+++ build.sh
@@ -2,17 +2,14 @@
 # Download and build tools needed for PIGA.  They are
 # - Margin
 # - Secphase
-# - PanGenie
 # - Beagle utilities
 # - MECAT2
 # - Merqury
-# - DeepVariant container
-
-
-set -beEu -o pipefail
+# - DeepVariant
 
 mainDir=$PWD
 buildDir=$PWD/piga_tools_build
+calllrDir=$PWD/scripts/call_lr_snv
 binDir=$CONDA_PREFIX/bin
 
 rm -rf ${buildDir}
@@ -23,7 +20,8 @@ cd ${buildDir}
 export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
 export LIBRARY_PATH=$CONDA_PREFIX/lib:$LIBRARY_PATH
 export PKG_CONFIG_PATH=$CONDA_PREFIX/lib/pkgconfig:$PKG_CONFIG_PATH
-
+export C_INCLUDE_PATH=$CONDA_PREFIX/include:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=$CONDA_PREFIX/include:$CPLUS_INCLUDE_PATH
 
 # Build Margin
 git clone https://github.com/UCSC-nanopore-cgl/margin.git --recursive
@@ -52,9 +50,13 @@ cd ${buildDir}
 
 git clone https://github.com/mobinasri/secphase.git
 cd secphase/programs/
+g++ -shared -fPIC -o submodules/edlib/libedlib.so submodules/edlib/edlib.cpp
+cp submodules/edlib/libedlib.so $CONDA_PREFIX/lib
+cp submodules/edlib/edlib.h $CONDA_PREFIX/include
 sed -i "s/\/home\/apps\/sonLib\/lib\/sonLib.a/$(echo ${buildDir} | sed 's/\//\\\//g')\/sonLib\/lib\/sonLib.a/g" Makefile
 sed -i "s/\/home\/apps\/sonLib\/C\/inc\//$(echo ${buildDir} | sed 's/\//\\\//g')\/sonLib\/C\/inc\//g" Makefile
 sed -i "s/\/usr\/local\/include\/htslib/$(echo ${buildDir} | sed 's/\//\\\//g')\/htslib\/include\/htslib\//g" Makefile
+sed -i "s/-Lsubmodules\/edlib/-Lsubmodules\/edlib -L$(echo ${CONDA_PREFIX}/lib | sed 's/\//\\\//g') -lstdc++/g" Makefile
 make
 mv bin/correct_bam ${binDir}
 mv bin/secphase ${binDir}
@@ -73,21 +75,21 @@ mv */bin/mecat2trimbases ${binDir}
 cd ${buildDir}
 
 
-# Build PanGenie
-git clone https://github.com/eblerjana/pangenie.git
-cd pangenie
-cmake .
-make -j 8
-mv src/PanGenie* ${binDir}
-cd ${buildDir}
-
-
 # Build cactus-gfa-tools
 git clone https://github.com/ComparativeGenomicsToolkit/cactus-gfa-tools.git
 cd cactus-gfa-tools
 make -j 8
 mv gaf2paf ${binDir}
 mv gaffilter ${binDir}
+cd ${buildDir}
+
+
+# Build ORCA
+git clone https://github.com/thocevar/orca.git
+cd orca
+g++ -O2 -std=c++11 -o orca.exe orca.cpp
+mv orca.exe ${binDir}
+cd ${buildDir}
 
 
 # Download Merqury
@@ -99,21 +101,21 @@ mv merqury.sh ${binDir}
 mv trio ${binDir}
 mv util ${binDir}
 mv eval ${binDir}
+cd ${buildDir}
 
 
 # Download Beagle utilities
 wget https://faculty.washington.edu/browning/beagle_utilities/splitvcf.jar
 chmod +x splitvcf.jar
-mv ./splitvcf.jar ${binDir}
+mv splitvcf.jar ${calllrDir}
 
 wget https://faculty.washington.edu/browning/beagle_utilities/mergevcf.jar
 chmod +x mergevcf.jar
-mv ./mergevcf.jar ${binDir}
-cd ${buildDir}
+mv mergevcf.jar ${calllrDir}
 
 
 # Download DeepVariant container
 singularity pull docker://google/deepvariant
-mv deepvariant*.sif ${mainDir}/config/deepvariant.sif
+mv deepvariant*.sif ${calllrDir}/deepvariant.sif
 cd ${mainDir}
 rm -rf ${buildDir}
