64c4191f1ad26eab1a6670699d6680bd8140f779
hahahafeifeifei <569631224@qq.com>
2025-12-03 01:56:51 +0800

    Test Data

--- test_data/download.sh
+++ test_data/download.sh
@@ -9,17 +9,22 @@ do
     mv ${sample}.* ${sample}
 done
 
+# GATK resource
+wget -c -i gatk_resource.url.txt
+mkdir reference/gatk_bundle
+mv *vcf* reference/gatk_bundle
+
 # Reference genome
 ## GRCh38
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/405/GCF_000001405.40_GRCh38.p14/GRCh38_major_release_seqs_for_alignment_pipelines/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
 gunzip GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.gz
+mv GCA_000001405.15_GRCh38_no_alt_analysis_set.fna reference
+samtools index reference/15_GRCh38_no_alt_analysis_set
 ## CHM13
 wget https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/analysis_set/chm13v2.0.fa.gz
 gunzip chm13v2.0.fa.gz
-
-mkdir reference
-mv GCA_000001405.15_GRCh38_no_alt_analysis_set.fna reference
 mv chm13v2.0.fa reference
+samtools index reference/chm13v2.0.fa
 
 # External pangenome (HGSVC3)
 wget https://ftp.1000genomes.ebi.ac.uk/vol1/ftp/data_collections/HGSVC3/release/Graph_Genomes/1.0/2024_02_23_minigraph_cactus_hgsvc3_hprc/hgsvc3-hprc-2024-02-23-mc-chm13.gbz
