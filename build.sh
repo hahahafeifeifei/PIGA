8ffaf7cc2180dc6f998331a99ed951eea114f72f
hahahafeifeifei <569631224@qq.com>
2026-01-29 11:47:46 +0800

    Update building script

--- build.sh
+++ build.sh
@@ -14,6 +14,7 @@ buildDir=$PWD/piga_tools_build
 calllrDir=$PWD/scripts/call_lr_snv
 inferDir=$PWD/scripts/infer_diploid_path
 binDir=$CONDA_PREFIX/bin
+thread=$(nproc | awk '{print ($1>8)?8:$1}')
 
 rm -rf ${buildDir}
 mkdir -p ${buildDir}
@@ -32,7 +33,7 @@ git clone https://github.com/UCSC-nanopore-cgl/margin.git --recursive
 cd margin
 export LDFLAGS="-L$CONDA_PREFIX/lib -ldeflate"
 cmake .
-make -j 8
+make -j ${thread}
 mv ./margin ${binDir}
 mv params/phase/allParams.phase_vcf.ont.sv.json ${inferDir}/margin.phase_sv.json
 mv params/base_params.json ${mainDir}/scripts/base_params.json
@@ -43,13 +44,13 @@ git clone https://github.com/samtools/htslib.git --recursive
 cd htslib
 autoreconf -i
 ./configure --prefix=$PWD
-make -j 8
+make -j ${thread}
 make install
 cd ${buildDir}
 
 git clone https://github.com/benedictpaten/sonLib.git
 cd sonLib
-make -j 8
+make -j ${thread}
 cd ${buildDir}
 
 git clone https://github.com/mobinasri/secphase.git
@@ -69,7 +70,7 @@ cd ${buildDir}
 # Build MECAT2
 git clone https://github.com/xiaochuanle/MECAT2.git
 cd MECAT2
-make -j 8
+make -j ${thread}
 mv */bin/mecat2map ${binDir}
 mv */bin/mecat2pm4 ${binDir}
 mv */bin/mecat2lcr ${binDir}
@@ -80,7 +81,7 @@ cd ${buildDir}
 # Build cactus-gfa-tools
 git clone https://github.com/ComparativeGenomicsToolkit/cactus-gfa-tools.git
 cd cactus-gfa-tools
-make -j 8
+make -j ${thread}
 mv gaf2paf ${binDir}
 mv gaffilter ${binDir}
 cd ${buildDir}
@@ -114,7 +115,17 @@ chmod +x mergevcf.jar
 mv mergevcf.jar ${calllrDir}
 
 # Download DeepVariant container
-singularity pull docker://google/deepvariant
+if singularity pull docker://google/deepvariant; then
+    echo "Download deepvariant successful."
+else
+    echo "Primary URL failed, trying backup URL."
+    if singularity pull docker://docker.1ms.run/google/deepvariant; then
+        echo "Download deepvariant successful."
+    else
+        echo "Download deepvariant failed."
+    fi
+fi
+
 mv deepvariant*.sif ${calllrDir}/deepvariant.sif
 cd ${mainDir}
 rm -rf ${buildDir}
